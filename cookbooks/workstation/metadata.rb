aa16e7e4fac09c26b025b634394a128f13b1f8b8
TomazMartins <tomaz.r.martins@gmail.com>
2019-06-23 21:26:10 -0300

    Adds empty cookbook: workstation

---
+++ cookbooks/workstation/metadata.rb
@@ -0,0 +1,8 @@
+name 'workstation'
+maintainer 'zamott'
+maintainer_email 'tomaz.r.martins@gmail.com'
+license 'All Rights Reserved'
+description 'Installs/Configures workstation'
+long_description 'Installs/Configures workstation'
+version '0.1.0'
+chef_version '>= 13.0'
